50f7e115894c3a767c72c7845ab3a5601b70616a
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 09:50:19 -0500

    debug macos build (clang problems)

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 set -x
+
+# debug====================
+xcode-select -p
+echo "$PATH" | tr ':' '\0' | xargs -0 -n 1
+brew info gcc
+which gcc
+which g++
+which as
+ls -l /usr/local/bin/gcc*
+ls -l /usr/local/bin/g++*
+xcode-select -p
+# =========================
+
 set -e
 
 root="$(pwd)"
@@ -12,8 +25,6 @@ cd tmp/cmake
 
 
 
-cmake "$root"
+cmake "$root" --debug-trycompile
 cmake --build .
 cpack
-
-ls -l
