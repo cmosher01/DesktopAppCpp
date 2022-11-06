eff98425987e6ab1d766c4ff06f057a6c077cfaa
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-05 20:51:48 -0400

    log standard directories and other app info; don't start GUI, just exit

--- src/FoobarApp.cpp
+++ src/FoobarApp.cpp
@@ -1,15 +1,68 @@
 #include "FoobarApp.h"
 #include "FoobarFrame.h"
+#include <wx/stdpaths.h>
 #include <boost/log/trivial.hpp>
 
 wxIMPLEMENT_APP_NO_MAIN(FoobarApp);
 
+#define StdDir(n) BOOST_LOG_TRIVIAL(info) << "    " #n " dir: " << wxStandardPaths::Get().Get##n##Dir()
+#define StdUserDir(n) BOOST_LOG_TRIVIAL(info) << "    User " #n " dir: " << wxStandardPaths::Get().GetUserDir(wxStandardPaths::Dir_##n)
+
 bool FoobarApp::OnInit()
 {
-    FoobarFrame *frame = new FoobarFrame();
-    frame->Show(true);
+    SetVendorName("nu.mine.mosher");
 
-    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+#ifdef __WXMSW__
+    wxStandardPaths::Get().DontIgnoreAppSubDir()
+#endif
+    wxStandardPaths::Get().SetFileLayout(wxStandardPaths::FileLayout_XDG);
+    wxStandardPaths::Get().UseAppInfo(wxStandardPaths::AppInfo_AppName | wxStandardPaths::AppInfo_VendorName);
 
-    return true;
+
+
+    BOOST_LOG_TRIVIAL(info) << "App name: " << GetAppName();
+    BOOST_LOG_TRIVIAL(info) << "App display name: " << GetAppDisplayName();
+    BOOST_LOG_TRIVIAL(info) << "Vendor name: " << GetVendorName();
+    BOOST_LOG_TRIVIAL(info) << "Vendor display name: " << GetVendorDisplayName();
+    BOOST_LOG_TRIVIAL(info) << "Class name: " << GetClassName();
+
+
+
+    BOOST_LOG_TRIVIAL(info) << "Install prefix: " << wxStandardPaths::Get().GetInstallPrefix();
+    BOOST_LOG_TRIVIAL(info) << "Executable path: " << wxStandardPaths::Get().GetExecutablePath();
+    BOOST_LOG_TRIVIAL(info) << "test config file name: " << wxStandardPaths::Get().MakeConfigFileName(wxString("test"));
+
+    BOOST_LOG_TRIVIAL(info) << "Standard directories";
+    StdDir(Plugins);
+    StdDir(Resources);
+    BOOST_LOG_TRIVIAL(info) << "    LocalizedResources (en) dir: " << wxStandardPaths::Get().GetLocalizedResourcesDir(wxString("en"), wxStandardPaths::ResourceCat_None);
+    StdDir(Config);
+    StdDir(UserConfig);
+
+    StdUserDir(Cache);
+
+    StdDir(Data);
+    StdDir(LocalData);
+    StdDir(UserData);
+    StdDir(UserLocalData);
+
+    StdDir(AppDocuments);
+    //StdDir(Documents); same as StdUserDir(Documents)
+    StdUserDir(Documents);
+    StdUserDir(Desktop);
+    StdUserDir(Downloads);
+    StdUserDir(Music);
+    StdUserDir(Pictures);
+    StdUserDir(Videos);
+
+    StdDir(Temp);
+
+
+
+
+
+    // FoobarFrame *frame = new FoobarFrame();
+    // frame->Show(true);
+    // return true;
+    return false;
 }
