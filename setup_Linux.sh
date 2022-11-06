3ad8a2067298042e387d9b42c938bd44c6c203ee
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 16:14:58 -0500

    add kinetic repo to ubuntu (for wxWidgets 3.2)

--- setup_Linux.sh
+++ setup_Linux.sh
@@ -2,4 +2,7 @@
 set -x
 
 sudo apt-get -qqqq update
+sudo apt-get -qqqq install software-properties-common
+sudo add-apt-repository "deb http://mirror.math.princeton.edu/pub/ubuntu kinetic main restricted universe multivers"
+sudo apt-get -qqqq update
 sudo apt-get -qqqq install libwxgtk3.2-dev
