0d14aa8869900ac38cf23344c8820c2103d627be
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 14:21:05 -0500

    fix windows setup

--- setup_Windows.sh
+++ setup_Windows.sh
@@ -4,13 +4,11 @@ set -x
 sudo pacman -S --noconfirm libcurl
 
 cd lib
-
 vWx=3.2.1
-curl -L -O wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/download/v$vWx/wxMSW-$vWx_vc14x_x64_Dev.7z"
-curl -L -O wxMSW_ReleaseDLL.7z "https://github.com/wxWidgets/wxWidgets/releases/download/v$vWx/wxMSW-$vWx_vc14x_x64_ReleaseDLL.7z"
-
+curl -L -O wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_Dev.7z"
 7z l wxMSW_Dev.7x
 7z x wxMSW_Dev.7x -owxmsw
-
+curl -L -O wxMSW_ReleaseDLL.7z "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_ReleaseDLL.7z"
 7z l wxMSW_ReleaseDLL.7x
 7z x wxMSW_ReleaseDLL.7x -owxmsw
+cd -
