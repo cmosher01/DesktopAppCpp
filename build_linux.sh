cc8b8b6fa8ccba83cd09c844d06a236f7a04a644
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 01:20:58 -0400

    fix boost build

--- build_linux.sh
+++ build_linux.sh
@@ -2,8 +2,6 @@
 
 set -x
 git submodule update --init --recursive
-ls -l lib/boost/stage/x64/Release/lib
-ls -l lib/boost/stage/x64/Release/lib/*
 mkdir -p tmp/cmake
 cd tmp/cmake || exit 1
 cmake ../.. || exit 1
