18be1918720d46eb8e806fdea60d25f75a3a7ac0
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 18:23:27 -0500

    fix boost

--- build_linux.sh
+++ build_linux.sh
@@ -5,7 +5,7 @@ set -e
 # move staged boost (from github action build-boost in build.yaml workflow)
 # to well-known location
 if [ -d .boost ] ; then
-    mv -nv .boost/stage/x64/Debug/lib/cmake/Boost-* lib/boost
+    mv -nv .boost/stage/x64/Debug/lib/cmake lib/boost
 fi
 
 mkdir -p tmp/cmake
