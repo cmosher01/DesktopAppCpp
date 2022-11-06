1460f7f7a43d410679cd267d008659dfc3edf695
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 16:18:46 -0500

    fix ubuntu build; add lib dir

--- setup_Linux.sh
+++ setup_Linux.sh
@@ -3,6 +3,6 @@ set -x
 
 sudo apt-get -qqqq update
 sudo apt-get -qqqq install software-properties-common
-sudo add-apt-repository "deb http://mirror.math.princeton.edu/pub/ubuntu kinetic main restricted universe multivers"
+sudo add-apt-repository "deb http://mirror.math.princeton.edu/pub/ubuntu kinetic main restricted universe multiverse"
 sudo apt-get -qqqq update
 sudo apt-get -qqqq install libwxgtk3.2-dev
