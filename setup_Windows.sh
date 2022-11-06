d9566755bcddd2738c8cc3c2f4451b2050e40f2e
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 14:35:27 -0500

    fix windows build

--- setup_Windows.sh
+++ setup_Windows.sh
@@ -2,17 +2,15 @@
 set -x
 set -e
 
-sudo pacman -S --noconfirm libcurl
-
 mkdir -p lib/wxmsw
 cd lib/wxmsw
 vWx=3.2.1
 echo '======================================================================================'
-curl -L -o wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_Dev.7z"
+curl -L -s -o wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_Dev.7z"
 7z l wxMSW_Dev.7x
 7z x wxMSW_Dev.7x -owxmsw
 echo '======================================================================================'
-curl -L -o wxMSW_ReleaseDLL.7z "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_ReleaseDLL.7z"
+curl -L -s -o wxMSW_ReleaseDLL.7z "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_ReleaseDLL.7z"
 7z l wxMSW_ReleaseDLL.7x
 7z x wxMSW_ReleaseDLL.7x -owxmsw
 echo '======================================================================================'
