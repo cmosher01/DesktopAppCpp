d90ee828b64629deafa4884a1b63e0b75942a124
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 14:36:39 -0500

    fix windows build

--- setup_Windows.sh
+++ setup_Windows.sh
@@ -11,7 +11,7 @@ curl -L -s -o wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/down
 7z x wxMSW_Dev.7x -owxmsw
 echo '======================================================================================'
 curl -L -s -o wxMSW_ReleaseDLL.7z "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_ReleaseDLL.7z"
-7z l wxMSW_ReleaseDLL.7x
-7z x wxMSW_ReleaseDLL.7x -owxmsw
+7z l wxMSW_ReleaseDLL.7z
+7z x wxMSW_ReleaseDLL.7z -owxmsw
 echo '======================================================================================'
 cd -
