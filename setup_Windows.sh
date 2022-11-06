acf70e63c137e214e4522aed77b6ecdfad2a7f48
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 14:53:37 -0500

    fix windows build

--- setup_Windows.sh
+++ setup_Windows.sh
@@ -6,11 +6,11 @@ mkdir -p lib
 cd lib
 vWx=3.2.1
 curl -L -s -o wxMSW_Dev.7x "https://github.com/wxWidgets/wxWidgets/releases/download/v${vWx}/wxMSW-${vWx}_vc14x_x64_Dev.7z"
-7z x wxMSW_Dev.7x -owxmsw
+7z x wxMSW_Dev.7x -owx
 
 ls -l
-ls -l wxmsw
-ls -l wxmsw/lib
-ls -l wxmsw/lib/vc14x_x64_dll
+ls -l wx
+ls -l wx/lib
+ls -l wx/lib/vc14x_x64_dll
 
 cd -
