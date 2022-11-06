7df7f848d1b2ceab36261ff38aa1a170f4aa8b32
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 10:03:28 -0500

    debug macos build (clang problems)

--- build_linux.sh
+++ build_linux.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-set -x
-
 # debug====================
+echo "---------------------xcode-select -p"
 xcode-select -p
+echo "---------------------PATH"
 echo "$PATH" | tr ':' '\0' | xargs -0 -n 1
+echo "---------------------brew info gcc"
 brew info gcc
+echo "---------------------which"
 which gcc
 which g++
 which as
+echo "---------------------ls -l /usr/local/bin/gcc*"
 ls -l /usr/local/bin/gcc*
+echo "---------------------ls -l /usr/local/bin/g++*"
 ls -l /usr/local/bin/g++*
-xcode-select -p
 # =========================
 
 set -e
 
 root="$(pwd)"
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
 mkdir -p tmp/cmake
 cd tmp/cmake
 
 
 
-cmake "$root" --debug-trycompile
-cmake --build .
-cpack
+cmake "$root" --debug-trycompile -D CMAKE_C_COMPILER=gcc-12 -D CMAKE_CXX_COMPILER=g++-12
+#cmake --build .
+#cpack
