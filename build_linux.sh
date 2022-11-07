7f25a4bc34bc401ded22ee3556c09d835f3def51
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-07 00:08:22 -0500

    fix boost

--- build_linux.sh
+++ build_linux.sh
@@ -1,47 +1,17 @@
 #!/bin/sh
-#set -e
-
-echo "====----------------------PATH----------------------===="
-echo "$PATH" | tr ':' '\0' | xargs -0 -n 1
-echo "====------------------------------------------------===="
-
-
-echo "/usr/lib==================================================="
-ls -l /usr/lib
-echo "/usr/lib/cmake==================================================="
-ls -l /usr/lib/cmake
-echo "/usr/local/lib==================================================="
-ls -l /usr/local/lib
-echo "/usr/local/lib/cmake==================================================="
-ls -l /usr/local/lib/cmake
-echo "/usr/lib/x86_64-linux-gnu==================================================="
-ls -l /usr/lib/x86_64-linux-gnu
-echo "/usr/lib/x86_64-linux-gnu/cmake==================================================="
-ls -l /usr/lib/x86_64-linux-gnu/cmake
-
-
 
 # move staged boost (from github action build-boost in build.yaml workflow)
 # to well-known location
 if [ -d lib/boost ] ; then
-    tree lib/boost
-
-    mkdir -p lib/cmake
-    cp -rv lib/boost/stage/x64/Debug/lib/* lib
-    ls -lr lib
+    mv lib/boost/boost include/
+    cp -rv lib/boost/stage/x64/Debug/lib/* lib/
 fi
 
 mkdir -p tmp/cmake
-cd tmp/cmake
+cd tmp/cmake || exit 1
 
-cmake ../..
-cmake --build .
-ls -l src
-
-
-exit 0
-
-
+cmake ../.. || exit 1
+cmake --build . || exit 1
 
 #cpack
 #ls -l
