1c1acf3d4b356b44cc538d97d82b8f54ecc6f5ef
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-06 23:00:12 -0500

    debug boost

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-set -e
+#set -e
+
+echo "MACHTYPE=$MACHTYPE"
 
 echo "====----------------------PATH----------------------===="
 echo "$PATH" | tr ':' '\0' | xargs -0 -n 1
@@ -7,10 +9,10 @@ echo "====------------------------------------------------===="
 
 
 echo "/usr/lib==================================================="
-ls /usr/lib
+ls -l /usr/lib
+echo "/usr/local/lib==================================================="
+ls -l /usr/local/lib
 
-echo "/usr/lib/x86_64-linux-gnu/cmake==================================================="
-ls -l /usr/lib/x86_64-linux-gnu/cmake
 
 
 # move staged boost (from github action build-boost in build.yaml workflow)
@@ -37,6 +39,11 @@ cd tmp/cmake
 cmake ../..
 cmake --build .
 ls -l src
+
+
+exit 0
+
+
 
 #cpack
 #ls -l
