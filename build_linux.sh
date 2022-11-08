e8e14f477b6b8409add8c1d78a2450f19f892d87
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-07 23:45:47 -0500

    try adding mingw to find paths

--- build_linux.sh
+++ build_linux.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo "PATH+"
+echo "PATH="
 echo "$PATH" | tr ':' '\0' | xargs -0 -n 1
 
 mkdir -p tmp/cmake
 cd tmp/cmake || exit 1
+rm -r *
 
 cmake ../.. || exit 1
 cmake --build .
