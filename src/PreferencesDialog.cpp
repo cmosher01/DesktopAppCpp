dd3cafe41974614d58fe743df67bc9a6d2cd27d8
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-26 14:34:36 -0500

    list built-in conf files to tree

--- src/PreferencesDialog.cpp
+++ src/PreferencesDialog.cpp
@@ -7,6 +7,8 @@
 #include "FoobarApp.h"
 #include <wx/xrc/xmlres.h>
 #include <wx/treectrl.h>
+#include <wx/dir.h>
+#include <wx/filename.h>
 
 wxBEGIN_EVENT_TABLE(PreferencesDialog, wxDialog)
 wxEND_EVENT_TABLE()
@@ -21,14 +23,44 @@ PreferencesDialog::~PreferencesDialog() {
 
 #define CTRL(t,n) t* n = XRCCTRL(*this, #n, t)
 
+class TreeSink : public wxDirTraverser {
+public:
+    TreeSink(wxTreeCtrl *tree, wxTreeItemId parent) : m_tree(tree), m_parent(parent) { }
+
+    wxDirTraverseResult OnFile(const wxString& filename)
+    {
+        wxFileName n = wxFileName::FileName(filename);
+        if (n.GetExt() == "conf") {
+            m_tree->AppendItem(m_parent, n.GetName());
+        }
+        return wxDIR_CONTINUE;
+    }
+
+    wxDirTraverseResult OnDir(const wxString& dirname) {
+        return wxDIR_CONTINUE;
+    }
+private:
+    wxTreeCtrl *m_tree;
+    wxTreeItemId m_parent;
+};
+
 void PreferencesDialog::OnInit() {
     wxXmlResource::Get()->LoadDialog(this, this->parent, "Preferences");
 
     this->SetSize(SIZ_DLG);
 
     CTRL(wxTreeCtrl, treItems);
-    wxTreeItemId configs = treItems->AddRoot("configurations");
-    wxTreeItemId built_in = treItems->AppendItem(configs, "built-in");
-    wxTreeItemId user = treItems->AppendItem(configs, "user");
+
+    wxTreeItemId configs = treItems->AddRoot(wxT("configurations"));
+    wxTreeItemId built_in = treItems->AppendItem(configs, wxT("built-in"));
+    wxTreeItemId user = treItems->AppendItem(configs, wxT("user"));
     treItems->Expand(configs);
+
+    TreeSink sinkBuiltIn(treItems, built_in);
+    wxDir dirBuiltIn = (wxDir)wxGetApp().GetResDir().c_str();
+    dirBuiltIn.Traverse(sinkBuiltIn, wxEmptyString, wxDIR_FILES | wxDIR_HIDDEN);
+    treItems->Expand(built_in);
+
+    // TODO user conf dir
+    treItems->Expand(user);
 }
