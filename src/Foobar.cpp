5bfc42b468f75a889731d72bf8538a627f90b680
Christopher A. Mosher <cmosher01@gmail.com>
2022-12-03 12:21:11 -0500

    improve wx status handling in main

--- src/Foobar.cpp
+++ src/Foobar.cpp
@@ -1,5 +1,10 @@
 #include <wx/app.h>
 
 int main(int argc, char *argv[]) {
-    return wxEntry(argc, argv);
+    const int status = wxEntry(argc, argv);
+    if (status < 0) {
+        return 1;
+    }
+
+    return status;
 }
